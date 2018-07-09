dcfb27d51452a92b816ccb10ac5b6f4ef919b402
divolitest@gmail.com <sreejiths@A10-2Sreejith.local>
2018-07-09 15:26:38 +0530

    first Commit

---
+++ SensotecCart/SensotecCart/SensotecCart.h
@@ -0,0 +1,19 @@
+//
+//  SensotecCart.h
+//  SensotecCart
+//
+//  Created by sreejith s on 09/07/18.
+//  Copyright © 2018 sreejith s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SensotecCart.
+FOUNDATION_EXPORT double SensotecCartVersionNumber;
+
+//! Project version string for SensotecCart.
+FOUNDATION_EXPORT const unsigned char SensotecCartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SensotecCart/PublicHeader.h>
+
+
---
+++ SensotecCart/SensotecCart/sensoCart.swift
@@ -0,0 +1,17 @@
+//
+//  sensoCart.swift
+//  SensotecCart
+//
+//  Created by sreejith s on 09/07/18.
+//  Copyright © 2018 sreejith s. All rights reserved.
+//
+
+import Foundation
+
+public class sensoCart {
+    
+    public static func sampleCart() -> String {
+        let myCart = "myCart"
+        return myCart
+    }
+}
